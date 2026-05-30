6b40e0e0c67f3b013bf7366e27ce7c7fa763b900
DASHYiii <artemsuh.artem@gmail.com>
2026-05-30 18:41:20 +0300

    Add files via upload
    Добавлен Flappy Bird со звуками

---
+++ winforms-flappy-bird-main/Flappy Bird/Flappy Bird/MenuForm.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Flappy_Bird
+{
+    partial class MenuForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnPlay = new Button();
+            btnAbout = new Button();
+            btnExit = new Button();
+            SuspendLayout();
+            // 
+            // btnPlay
+            // 
+            btnPlay.BackColor = Color.FromArgb(128, 255, 128);
+            btnPlay.FlatStyle = FlatStyle.Flat;
+            btnPlay.Font = new Font("Arial Narrow", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            btnPlay.Location = new Point(97, 147);
+            btnPlay.Name = "btnPlay";
+            btnPlay.Size = new Size(175, 56);
+            btnPlay.TabIndex = 0;
+            btnPlay.Text = "Играть";
+            btnPlay.UseVisualStyleBackColor = false;
+            btnPlay.Click += btnPlay_Click;
+            // 
+            // btnAbout
+            // 
+            btnAbout.BackColor = Color.FromArgb(128, 255, 128);
+            btnAbout.FlatStyle = FlatStyle.Flat;
+            btnAbout.Font = new Font("Arial Narrow", 13.8F, FontStyle.Bold);
+            btnAbout.Location = new Point(97, 227);
+            btnAbout.Name = "btnAbout";
+            btnAbout.Size = new Size(175, 56);
+            btnAbout.TabIndex = 1;
+            btnAbout.Text = "О игре";
+            btnAbout.UseVisualStyleBackColor = false;
+            btnAbout.Click += btnAbout_Click;
+            // 
+            // btnExit
+            // 
+            btnExit.BackColor = Color.FromArgb(128, 255, 128);
+            btnExit.FlatStyle = FlatStyle.Flat;
+            btnExit.Font = new Font("Arial Narrow", 13.8F, FontStyle.Bold);
+            btnExit.Location = new Point(97, 305);
+            btnExit.Name = "btnExit";
+            btnExit.Size = new Size(175, 56);
+            btnExit.TabIndex = 2;
+            btnExit.Text = "Выйти";
+            btnExit.UseVisualStyleBackColor = false;
+            btnExit.Click += btnExit_Click;
+            // 
+            // MenuForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.background;
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(382, 553);
+            Controls.Add(btnExit);
+            Controls.Add(btnAbout);
+            Controls.Add(btnPlay);
+            DoubleBuffered = true;
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "MenuForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Flappy Bird: Меню";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnPlay;
+        private Button btnAbout;
+        private Button btnExit;
+    }
+}
---
+++ winforms-flappy-bird-main/Flappy Bird/Flappy Bird/MenuForm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Flappy_Bird
+{
+    public partial class MenuForm : Form
+    {
+        public MenuForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            Form1 game = new Form1();
+            this.Hide();
+            game.ShowDialog();
+            this.Show();
+        }
+
+        private void btnAbout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(
+                "Flappy Bird: Special Edition\n\n" +
+                "Разработчики:\n" +
+                "- Slime\n" +
+                "- DASH \n\n" +
+                "Управление:\n" +
+                "Пробел — Прыжок и Рестарт.\n\n" +
+                "Удачи в достижении новых рекордов!", 
+                "Об игре");
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
